760f7124479da6b479914890195df30c5624194f
OakleafOne <oliver.ekbladd@gmail.com>
2026-05-28 12:40:33 +0200

    Started with the login and register pages by adding their components.

---
+++ API-minigame/src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {useState} from "react";
+
+const Register = ({setView}) => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    
+
+}
